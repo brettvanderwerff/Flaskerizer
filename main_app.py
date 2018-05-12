44a9ffc42f5fdebceb525dd6d0d1a29ff0dbc770
Brett.Vanderwerff <vande060@gmail.com>
2018-05-12 16:36:10 -0700

    add main loop

--- main_app.py
+++ main_app.py
@@ -1,9 +1,10 @@
+import os
 from structure_directory import StructureDirectory
 from write_app import WriteApp
 
-flaskerizer_object = StructureDirectory(directroy=r'C:\Users\vande060\Desktop\coding\projects\Flaskerizer\Sailor')
-write_app_object = WriteApp()
-
-flaskerizer_object.migrate_static()
-flaskerizer_object.parse_html()
-write_app_object.write_app()
+if __name__ == '__main__':
+    flaskerizer_object = StructureDirectory(directroy=os.path.join(os.getcwd(), os.path.basename('Folio')))
+    write_app_object = WriteApp()
+    flaskerizer_object.migrate_static()
+    flaskerizer_object.parse_html()
+    write_app_object.write_app()
