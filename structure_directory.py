fc59137c52bb8b21dd1c2b6ba8398741fb12dd3e
Brett Vanderwerff <34728357+brettvanderwerff@users.noreply.github.com>
2018-05-12 15:18:39 -0700

    Renamed flaskerizer.py to structure_directory.py

---
+++ structure_directory.py
@@ -0,0 +1,51 @@
+import os
+import shutil
+
+class StructureDirectory():
+    def __init__(self, directroy):
+        self.directory = directroy
+
+    def mkdir(self, dir):
+        '''Makes folder of dir name in the working direectory.
+        '''
+        dir_path = os.path.join(os.getcwd(), os.path.basename(dir))
+        if not os.path.exists(dir_path):
+            print('generating {} folder'.format(dir))
+            os.makedirs(dir_path)
+
+    def migrate_static(self):
+        #shutil.rmtree(os.path.join(os.getcwd(), os.path.basename('static'))) #uncomment to overwrite
+        self.mkdir('static')
+        for item in os.listdir(self.directory):
+            print('migrating {} to static folder'.format(item))
+            item_path = os.path.join(self.directory, os.path.basename(item))
+            if os.path.isdir(item_path):
+                shutil.copytree(item_path,
+                                os.path.join(os.getcwd(), os.path.basename('static'), os.path.basename(item)))
+
+    def migrate_templates(self, html_content, file_name):
+        write_directory = os.path.join(os.getcwd(), os.path.basename('templates'), os.path.basename(file_name))
+        for line in html_content:
+            with open(write_directory, 'a') as write_obj:
+                for folder in os.listdir(os.path.join(os.getcwd(), os.path.basename('static'))):
+                    if ('=\"' + str(folder) + "/") in line:
+                        split_line = line.split("\"" + str(folder) + "/")
+                        line = ("\"" + '/static/' + (str(folder) + "/")).join(split_line)
+                write_obj.write(line)
+
+    def parse_html(self):
+        #shutil.rmtree(os.path.join(os.getcwd(), os.path.basename('templates')))
+        self.mkdir('templates')
+        for file_name in os.listdir(self.directory):
+            if '.html' in file_name:
+                print('generating content for {} and migrating content to templates folder'.format(file_name))
+                source_directory = os.path.join(self.directory, os.path.basename(file_name))
+                with open(source_directory) as html_content:
+                    self.migrate_templates(html_content, file_name)
+
+if __name__ == "__main__":
+    my_object = StructureDirectory(directroy=r'C:\Users\vande060\Desktop\coding\projects\Flaskerizer\Folio')
+    my_object.migrate_static()
+    my_object.parse_html()
+
+
