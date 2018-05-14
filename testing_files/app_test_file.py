c3f806d4abd569613fabda554130710442c86ba6
Brett.Vanderwerff <vande060@gmail.com>
2018-05-14 08:01:33 -0700

    addd gold standard testing files

---
+++ testing_files/app_test_file.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/blog-grid.html')
+def blog_grid():
+    return render_template('blog-grid.html')
+
+@app.route('/blog-single.html')
+def blog_single():
+    return render_template('blog-single.html')
+
+@app.route('/index.html')
+def index():
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run()
