f72c3924c79f7429c17b5dff2c8e72bd3b97e23e
Brett.Vanderwerff <vande060@gmail.com>
2018-05-14 20:34:42 -0700

    added docstrings

--- write_app.py
+++ write_app.py
@@ -5,9 +5,16 @@ from status_code_to_word import status_code_to_word
 class WriteApp():
 
     def get_routes(self):
+        '''Gets the name of every HTML template in the templates folder.
+        '''
         return [template for template in os.listdir(os.path.join(os.getcwd(), os.path.basename('templates')))]
 
-    def write_error_handler(self, template_name, write_obj): #ToDo eliminate non-error status codes from dictionary
+    def write_error_handler(self, template_name, write_obj):#ToDo eliminate non-error status codes from dictionary
+        '''If the write_routes function detects a template name as containing a status code, the template name will
+        be passed to write_error_handler along with the write_obj. The write_error_handler function will write an
+        error handler for the template, which is making the assumption that the template was intended to represent
+        an error code.
+        '''
         status_code = template_name.strip('.html')
         function_name = status_code_to_word(status_code)
         write_obj.write('@app.errorhandler({})\n'.format(status_code))
@@ -15,6 +22,10 @@ class WriteApp():
         write_obj.write("    return render_template('{}'), {}\n\n".format(template_name, status_code))
 
     def write_routes(self, write_obj):
+        '''For every HTML template in the 'templates' folder, write_routes generates a function that is bound to the
+        URL for that template by the route() decorator. See http://flask.pocoo.org/docs/1.0/quickstart/#routing
+        for more info.
+        '''
         for template_name in self.get_routes():
             if template_name.strip('.html') in HTTP_status_dict.keys():
                 self.write_error_handler(template_name, write_obj)
@@ -24,6 +35,11 @@ class WriteApp():
             write_obj.write("    return render_template('{}')\n\n".format(template_name))
 
     def write_app(self):
+        '''Writes the 'skeleton' of a Flask app in a file 'app.py'. Writes import statements for Flask and
+        render_template, instantiates an object 'app' from the 'Flask' class, and generates a conditional
+        'if __name__ == '__main__':' to run the Flask app.
+         URL routes are added to the 'skeleton' by calling the write_routes method.
+         '''
         with open('app.py', 'w') as write_obj:
             write_obj.write('from flask import Flask, render_template\n\n')
             write_obj.write('app = Flask(__name__)\n\n')
