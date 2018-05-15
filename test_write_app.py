4e38224187c495bed8e3b6c2dc9e1728f18078ea
Brett.Vanderwerff <vande060@gmail.com>
2018-05-14 20:34:25 -0700

    updated docstrings

--- test_write_app.py
+++ test_write_app.py
@@ -4,23 +4,29 @@ from structure_directory import StructureDirectory
 from write_app import WriteApp
 
 class TestWriteApp(unittest.TestCase):
+    maxDiff = None # reveals difference between test strings and "gold standard" strings
+
     def setUp(self):
-        flaskerizer_object = StructureDirectory(directroy=os.path.join(os.getcwd(), os.path.basename('Folio')))
-        flaskerizer_object.migrate_static()
-        flaskerizer_object.parse_html()
+        '''Instantiates a 'structure_directory_object' from the StructureDirectory class. The path to the Bootstrap
+        template 'Folio' is given as an argument for testing purposes. The object 'test' is also instantiated from the
+        WhiteApp class.
+        '''
+        structure_directory_object = StructureDirectory(directroy=os.path.join(os.getcwd(), os.path.basename('Folio')))
+        structure_directory_object.migrate_static()
+        structure_directory_object.parse_html()
+        self.test = WriteApp()
 
     def test_get_routes(self):
-        '''Tests that the get_routes function returns a list.
+        '''Tests that the get_routes method of the WriteApp class returns a list.
         '''
-        test = WriteApp()
-        self.assertIsInstance(test.get_routes(), list)
+        self.assertIsInstance(self.test.get_routes(), list)
 
     def test_write_app(self):
-        ''' Tests that write_app creates a file app.py that matches a "gold standard"  app.py called
-        app_test_file.py from the testing_files directory. These two files are compared line for line.
+        ''' Tests that write_app method of the WriteApp class creates a file app.py that matches a "gold standard"
+        app.py called app_test_file.py from the testing_files directory. These two files are compared line for line
+        in the test.
         '''
-        test = WriteApp()
-        test.write_app()
+        self.test.write_app()
         with open('app.py', 'r') as test_obj:
             test_string = test_obj.read()
         test_dir = os.path.join(os.getcwd(), os.path.basename('testing_files'), os.path.basename('app_test_file.py'))
