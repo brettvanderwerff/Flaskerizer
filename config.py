e40500c54523753747bab7fa5f118179b93617d9
Brett Vanderwerff <34728357+brettvanderwerff@users.noreply.github.com>
2018-05-19 09:23:33 -0700

    Update config.py
    Docstrings can't be placed inside dictionaries, it causes an exception.

--- config.py
+++ config.py
@@ -1,9 +1,10 @@
 import os
 
-CONFIGURATION = {
-       ''' enter the full path of downloaded bootstrap template as the value of directory_path key.
+''' enter the full path of downloaded bootstrap template as the value of directory_path key.
             Currently 'Folio_example' is the default template and you can continue with it or replace it 
             with the path of your downloaded bootstrap template 
        '''
+
+CONFIGURATION = {
        'directory_path': os.path.join(os.getcwd(), os.path.basename('Folio_example'))
 }
