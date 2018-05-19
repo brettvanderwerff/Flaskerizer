99f9d4bc76fe0f81f6ee355867ee7fa3b687c098
Mayank Nader <mayank.nader11@gmail.com>
2018-05-19 14:23:34 +0530

    make config user friendly

--- config.py
+++ config.py
@@ -1,6 +1,9 @@
 import os
 
 CONFIGURATION = {
-        # enter the full path of downloaded bootstrap template as the value of directory_path key
-        'directory_path': os.path.join(os.getcwd(), os.path.basename('Folio_example'))
+       ''' enter the full path of downloaded bootstrap template as the value of directory_path key.
+            Currently 'Folio_example' is the default template and you can continue with it or replace it 
+            with the path of your downloaded bootstrap template 
+       '''
+       'directory_path': os.path.join(os.getcwd(), os.path.basename('Folio_example'))
 }
