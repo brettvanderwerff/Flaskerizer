82cc09073cb795ccac357bec94a104dc1b8aade2
Brett.Vanderwerff <vande060@gmail.com>
2018-05-20 13:33:56 -0700

    added static and template route attributes to StructureDirectory

--- structure_directory.py
+++ structure_directory.py
@@ -1,48 +1,18 @@
-import ntpath
 import os
-import config
+from config import CONFIGURATION
 import shutil
-from zipfile import ZipFile
-
-directory = config.CONFIGURATION['directory_path']
-
 
 class StructureDirectory():
-    def __init__(self, directory):
-        '''__init__ method defines the 'directory' attribute of the StructureDirectory class, which is a path to a
-        Bootstrap template given by the 'directory' argument. If the 'directory' argument is a path to an unzipped
-        Bootstrap template folder, the unzip method of the StructureDirectory class unzips the Bootstrap template to
-        the top level directory of the repository and defines the 'directory' attribute as a path that points to the
-        newly unzipped Bootstrap template folder in the top level directory of the repository.
+    def __init__(self, templates_path, static_path):
+        '''Instantiates the StructureDirectory object with the templates_path attribute that is a path to the HTML
+        files in the Bootstrap template source folder that will be migrated to the Flask 'templates' folder. Also
+        instantiates the StructureDirectory object with the static_path attribute that is a path to the css,
+        javascript, images, fonts, etc. content of the Bootstrap template source folder that will be migrated to
+        the Flask 'static' folder.
         '''
-        if self.is_zip(directory) == True:
-            self.unzip(directory)
-            self.directory = os.path.join(os.getcwd(), ntpath.basename(directory).strip('.zip'))
-        elif self.is_zip(directory) == False:
-            self.directory = directory
+        self.templates_path = templates_path
+        self.static_path = static_path
 
-    def is_zip(self, directory):
-        '''Returns True if the directory argument is the path of a '.zip' file. If the directory argument is a file
-        other than a '.zip' file, the function will print an informative error statement before exiting the program.
-        If the directory argument is the path of a folder, the is_zip method will return False.
-         '''
-        print('checking if bootstrap template is a zipped file')
-        if os.path.isfile(directory) and (directory[-4:]) == '.zip':
-            return True
-        elif os.path.isfile(directory):
-            print('file type not recognized, please verify that it is either an unzipped Bootstrap template folder'
-                  ' or a zipped Bootstrap template folder before trying again.')
-            exit()
-        elif os.path.isdir(directory):
-            return False
-
-    def unzip(self, directory):
-        '''Unzips a file at path 'directory'
-        '''
-        print('unzipping {}'.format(ntpath.basename(directory)))
-        with ZipFile(directory, 'r') as zip_obj:
-            zip_obj.extractall(os.getcwd())
-            
     def mkdir(self, dir):
         '''Makes folder of dir name in the working directory.
         '''
@@ -60,9 +30,9 @@ class StructureDirectory():
         the static folder (css, js, etc) to the newly made static folder.
         '''
         self.mkdir('static')
-        for item in os.listdir(self.directory):
+        for item in os.listdir(self.static_path):
             print('migrating {} to static folder'.format(item))
-            item_path = os.path.join(self.directory, os.path.basename(item))
+            item_path = os.path.join(self.static_path, os.path.basename(item))
             if os.path.isdir(item_path):
                 shutil.copytree(item_path,
                                 os.path.join(os.getcwd(), os.path.basename('static'), os.path.basename(item)))
@@ -85,16 +55,17 @@ class StructureDirectory():
         '''Locates all the HTML files in the Bootstrap template directory.
         '''
         self.mkdir('templates')
-        for file_name in os.listdir(self.directory):
+        for file_name in os.listdir(self.templates_path):
             if '.html' in file_name:
                 print('generating content for {} and migrating content to templates folder'.format(file_name))
-                source_directory = os.path.join(self.directory, os.path.basename(file_name))
+                source_directory = os.path.join(self.templates_path, os.path.basename(file_name))
                 with open(source_directory) as html_content:
                     self.migrate_templates(html_content, file_name)
 
 
 if __name__ == "__main__":
-    my_object = StructureDirectory(directory)
+    my_object = StructureDirectory(templates_path=CONFIGURATION['templates_path'],
+                                   static_path=CONFIGURATION['static_path'])
     my_object.migrate_static()
     my_object.parse_html()
 
