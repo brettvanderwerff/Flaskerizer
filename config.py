f7d229608d9999458a1be12709068fc39e48730d
Brett.Vanderwerff <vande060@gmail.com>
2018-05-20 13:33:19 -0700

    added static and templates route configurations

--- config.py
+++ config.py
@@ -1,9 +1,25 @@
 import os
 
 ''' 
-Enter the full path of downloaded bootstrap template as the value of directory_path key.
-Currently 'Folio_example' is the default template and you can continue with it or replace it 
-with the path of your downloaded bootstrap template 
+After unzipping your Bootstrap template, open the template and find the folder that contains the HTML files. Copy the 
+full path to this folder to the 'templates_path' value in the COFIGURATION dictionary below. These HTML files will be 
+migrated to the Flask 'templates' folder by Flaskerizer. 
+
+Note that there is sometimes more than one folder containing HTML files in bootstrap templates. 
+Choose the one folder that does not contain any rerouting HTML files (hint this folder will usually have the most 
+HTML files in it). Also note that this 'templates_path' may simply be the top level directory of the bootstrap
+template as it is in the 'Folio_example'
+ 
+Next find the folder within the boostrap template that contains all the css, javascript, images, etc. Copy the 
+full path to this folder to the 'static_path' value in the COFIGURATION dictionary below. These HTML files will be 
+migrated to the Flask 'static' folder by Flaskerizer.
+
+Note that this 'static_path' may also simply be the top level directory of the bootstrap template as it is in the 
+'Folio_example'
+  
+Currently the default 'templates_path' and 'static_path' values are set to reflect paths
+that are correcet for the 'Folio_example', you should replace these values with the paths to your unzipped 
+Bootstrap template of choice. 
 '''
 
 CONFIGURATION = {
