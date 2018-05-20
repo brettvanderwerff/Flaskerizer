dcf4d08b966a53bf62e427cbf3d2f4b8d3a7fe10
Brett.Vanderwerff <vande060@gmail.com>
2018-05-20 11:34:34 -0700

    updated config.py to have templates and static values

--- config.py
+++ config.py
@@ -1,11 +1,12 @@
 import os
 
 ''' 
-enter the full path of downloaded bootstrap template as the value of directory_path key.
+Enter the full path of downloaded bootstrap template as the value of directory_path key.
 Currently 'Folio_example' is the default template and you can continue with it or replace it 
 with the path of your downloaded bootstrap template 
 '''
 
 CONFIGURATION = {
-       'directory_path': os.path.join(os.getcwd(), os.path.basename('Folio_example'))
+       'templates_path': os.path.join(os.getcwd(), os.path.basename('Folio_example')),
+       'static_path' : os.path.join(os.getcwd(), os.path.basename('Folio_example'))
 }
