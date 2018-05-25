7a441a03ef1f98ed57797f9b7b32d7349f9ca6e5
Brett Vanderwerff <34728357+brettvanderwerff@users.noreply.github.com>
2018-05-24 17:49:36 -0700

    replace folio with alstar

--- config.py
+++ config.py
@@ -8,20 +8,20 @@ eventually be migrated to the Flask 'templates' folder by Flaskerizer.
 Note that there is sometimes more than one folder containing HTML files in bootstrap templates. 
 Choose the one folder that does not contain any rerouting HTML files (hint this folder will usually have the most 
 HTML files in it). Also note that this 'templates_path' may simply be the top level directory of the bootstrap
-template as it is in the 'Folio_example'
+template as it is in the 'Alstar_example'
  
 Next find the folder within the Bootstrap template that contains all the css, javascript, images, etc. Copy the 
 full path to this folder to the 'static_path' key value in the CONFIGURATION dictionary below. These HTML files will 
 eventually be migrated to the Flask 'static' folder by Flaskerizer.
 
 Note that this 'static_path' may also simply be the top level directory of the Bootstrap template as it is in the 
-'Folio_example'
+'Alstar_example'
 
 Finally, find the folder within the Bootstrap template that specifically contains the javascript and copy the full path 
 to this folder to the 'javascript_path' key value in the CONFIGURATION dictionary below.  
   
 Currently the default 'templates_path', 'static_path', and 'javascript_path' key values are set to reflect paths
-that are correct for the 'Folio_example', you should replace these values with the paths to your unzipped 
+that are correct for the 'Alstar_example', you should replace these values with the paths to your unzipped 
 Bootstrap template of choice. 
 '''
 
