a1db64357a45b035b02f6a44113f57cabc97e370
Brett Vanderwerff <34728357+brettvanderwerff@users.noreply.github.com>
2018-06-09 08:48:48 -0700

    added setUp method

--- Flaskerizer_src/Tests/test_gui_for_flaskerizer.py
+++ Flaskerizer_src/Tests/test_gui_for_flaskerizer.py
@@ -3,22 +3,27 @@ import os
 from Flaskerizer_src.gui_for_flaskerizer import ChooseFilesGUI
 
 class TestGUI(unittest.TestCase):
+
+    def setUp(self):
+        '''Instantiates an object 'test' from the ChoseFilesGUI class.
+        '''
+        self.test = ChooseFilesGUI(True)
+
     def test_values(self):
         """Gets the default values from the html,
         static and js string variables in the ChooseFilesGUI. Then
         sets new ones and tests to see if they are fully
         functional.
         """
-        self.testclass = ChooseFilesGUI(True)
-        self.assertEqual(self.testclass.get_values(),
-         ["Select one HTML file from the template",
+        self.assertEqual(self.test.get_values(),
+                         ["Select one HTML file from the template",
          "Select the template folder containing all the css, img, js folders",
          "Select one JavaScript (.js) file from the template JavaScript folder"])
-        self.testclass.html_location.set("Html Location")
-        self.testclass.static_location.set("Static Location")
-        self.testclass.js_location.set("JS Location")
-        self.assertEqual(self.testclass.get_values(),
-         ['Html Location', "Static Location",
+        self.test.html_location.set("Html Location")
+        self.test.static_location.set("Static Location")
+        self.test.js_location.set("JS Location")
+        self.assertEqual(self.test.get_values(),
+                         ['Html Location', "Static Location",
          "JS Location"])
 
     def test_path(self):
@@ -27,8 +32,7 @@ class TestGUI(unittest.TestCase):
         and compares it to this directory path.
         """
         self.this_directory = os.path.dirname(os.path.abspath(__file__))
-        self.test_class = ChooseFilesGUI(True)
-        self.path_to_folder = self.test_class.path_to_folder
+        self.path_to_folder = self.test.path_to_folder
         self.assertEqual(self.path_to_folder(self.this_directory + "/gui_for_flaskerizer.py"), (self.this_directory))
         self.assertEqual(self.path_to_folder(self.this_directory + "/flaskerizer.py"), (self.this_directory))
 
