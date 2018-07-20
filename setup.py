454aa5859d79df7193e65ff2e3b1d612447ae223
Brett.Vanderwerff <vande060@gmail.com>
2018-07-19 17:12:58 -0700

    add entry point test

--- setup.py
+++ setup.py
@@ -14,16 +14,21 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/brettvanderwerff/Flaskerizer",
     packages=setuptools.find_packages(),
-classifiers=(
-    "Programming Language :: Python :: 2.7",
-    "Programming Language :: Python :: 3.3",
-    "Programming Language :: Python :: 3.4",
-    "Programming Language :: Python :: 3.5",
-    "Programming Language :: Python :: 3.6",
-    "Programming Language :: Python :: 3.7",
-    "License :: OSI Approved :: MIT License",
-    "Operating System :: OS Independent",
-    "License :: OSI Approved :: GNU General Public License v2 (GPLv2)",
-    "Framework :: Flask",
-    "Operating System :: OS Independent"
-    ))
+    entry_points={
+              'console_scripts': [
+                  'flaskerizer = flaskerizer.__main__:main'
+              ]
+          },
+    classifiers=(
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3.3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "License :: OSI Approved :: GNU General Public License v2 (GPLv2)",
+        "Framework :: Flask",
+        "Operating System :: OS Independent"
+        ))
