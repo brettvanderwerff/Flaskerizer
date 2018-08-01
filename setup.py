c289f816758b979e369df496fd1f4c7eabae8b10
brett vanderwerff <vande060@gmail.com>
2018-07-31 21:32:38 -0700

    removed MIT license

--- setup.py
+++ setup.py
@@ -27,7 +27,6 @@ setuptools.setup(
         "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
-        "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
         "License :: OSI Approved :: GNU General Public License v2 (GPLv2)",
         "Framework :: Flask",
